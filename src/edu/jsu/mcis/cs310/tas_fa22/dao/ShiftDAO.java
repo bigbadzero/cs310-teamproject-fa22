1f528884738ed1e2dfab7500eaf832a3d214e02a
adam watson <adamwatson08@gmail.com>
2022-10-14 21:04:03 -0500

    added find shift by badge

--- src/edu/jsu/mcis/cs310/tas_fa22/dao/ShiftDAO.java
+++ src/edu/jsu/mcis/cs310/tas_fa22/dao/ShiftDAO.java
@@ -74,4 +74,64 @@ public class ShiftDAO {
 
     }
 
+    public Shift find(Badge badge){
+        Shift shift = null;
+
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try{
+            Connection conn = daoFactory.getConnection();
+
+            if(conn.isValid(0)){
+                ps = conn.prepareStatement(QUERY_FIND);
+                ps.setInt(1, Integer.parseInt(badge.getId()));
+
+                boolean hasResults = ps.execute();
+
+                if(hasResults){
+                    rs = ps.getResultSet();
+
+                    while(rs.next()){
+                        ShitMap.put("id", rs.getString("id"));
+                        ShitMap.put("description",rs.getString("description"));
+                        ShitMap.put("shiftStart",rs.getString("shiftstart"));
+                        ShitMap.put("shiftStop",rs.getString("shiftstop"));
+                        ShitMap.put("roundInterval",rs.getString("roundinterval"));
+                        ShitMap.put("gracePeriod",rs.getString("graceperiod"));
+                        ShitMap.put("dockPenalty",rs.getString("dockpenalty"));
+                        ShitMap.put("lunchStart",rs.getString("lunchstart"));
+                        ShitMap.put("lunchStop",rs.getString("lunchstop"));
+                        ShitMap.put("lunchThreshold",rs.getString("lunchthreshold"));
+                        shift = new Shift(ShitMap);
+
+                    }
+                }
+            }
+        }catch (SQLException e) {
+
+            throw new DAOException(e.getMessage());
+
+        } finally {
+
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                    throw new DAOException(e.getMessage());
+                }
+            }
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                    throw new DAOException(e.getMessage());
+                }
+            }
+
+        }
+
+        return shift;
+    }
+
 }
